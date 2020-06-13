2522f8c9ab342bad327945e11ea063f8ac0f19d6
unknown <jamesonkanakulya@gmail.com>
2020-06-13 18:16:37 +0400

    Updates

---
+++ precache-manifest.6428dce85bdc044bf11abdd07c6a2bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476adceeddba8f1869fe833965866d97",
+    "url": "/Jamesonkanakulya/firstProject.git/index.html"
+  },
+  {
+    "revision": "1f1e75935dc1d6b0ef15",
+    "url": "/Jamesonkanakulya/firstProject.git/static/css/main.6e09a407.chunk.css"
+  },
+  {
+    "revision": "5a227fa9c1e2998f6705",
+    "url": "/Jamesonkanakulya/firstProject.git/static/js/2.de28c021.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jamesonkanakulya/firstProject.git/static/js/2.de28c021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f1e75935dc1d6b0ef15",
+    "url": "/Jamesonkanakulya/firstProject.git/static/js/main.07fc0184.chunk.js"
+  },
+  {
+    "revision": "05595e6e5db4efe9b889",
+    "url": "/Jamesonkanakulya/firstProject.git/static/js/runtime-main.e22890f9.js"
+  }
+]);
